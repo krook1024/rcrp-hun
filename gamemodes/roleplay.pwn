krook1024/rcrp-hun

commit f2063a66d01e658988a59968019010fde8e9b214

Exclude houses in interiors from /buy house count check
This is so that players with apartment complexes can own all the apartments in the complex.

gamemodes/roleplay.pwn
@@ -7553,6 +7553,22 @@ House_GetCount(playerid)
 	return count;
 }
 
+// This alternative function ignores houses in interiors so that apartment complex owners can buy multiple apartments in their complexes
+House_GetCountEx(playerid)
+{
+	new
+		count = 0;
+
+	for (new i = 0; i != MAX_HOUSES; i ++)
+	{
+		if (HouseData[i][houseExists] && House_IsOwner(playerid, i) && HouseData[i][houseInterior] == 0 )
+		{
+			count++;
+		}
+	}
+	return count;
+}
+
 Business_GetCount(playerid)
 {
 	new
@@ -30080,7 +30096,7 @@ CMD:buy(playerid, params[])
 	}
 	else if ((id = Business_Nearest(playerid)) != -1)
 	{
-	    if (Business_GetCount(playerid) >= MAX_OWNABLE_BUSINESSES)
+	    if (Business_GetCountEx(playerid) >= MAX_OWNABLE_BUSINESSES)
 			return SendErrorMessage(playerid, "Egyszerre csak %d üzleted lehet.", MAX_OWNABLE_BUSINESSES);
 
 		if (BusinessData[id][bizOwner] != 0)

commit 3347cd3cbbef63b24625af790f9ccbf6c3744666

Fix a bug in f2063a6
f2063a6 accidentally applied the bugfix to businesses instead of houses. This commit fixes this.

gamemodes/roleplay.pwn
@@ -7538,6 +7538,7 @@ Car_GetCount(playerid)
 	return count;
 }
 
+#pragma unused House_GetCount
 House_GetCount(playerid)
 {
 	new
@@ -30074,7 +30075,7 @@ CMD:buy(playerid, params[])
 
 	if ((id = House_Nearest(playerid)) != -1)
 	{
-		if (House_GetCount(playerid) >= MAX_OWNABLE_HOUSES)
+		if (House_GetCountEx(playerid) >= MAX_OWNABLE_HOUSES)
 			return SendErrorMessage(playerid, "Egyszerre csak %d házad lehet.", MAX_OWNABLE_HOUSES);
 
 		if (HouseData[id][houseOwner] != 0)
@@ -30096,7 +30097,7 @@ CMD:buy(playerid, params[])
 	}
 	else if ((id = Business_Nearest(playerid)) != -1)
 	{
-	    if (Business_GetCountEx(playerid) >= MAX_OWNABLE_BUSINESSES)
+	    if (Business_GetCount(playerid) >= MAX_OWNABLE_BUSINESSES)
 			return SendErrorMessage(playerid, "Egyszerre csak %d üzleted lehet.", MAX_OWNABLE_BUSINESSES);
 
 		if (BusinessData[id][bizOwner] != 0)